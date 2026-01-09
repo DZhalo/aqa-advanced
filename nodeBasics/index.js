a9a2c956ac3aa84b19f8dca4f8a4dcecbf1afb3a
Daria Zhaloba <d.zhaloba@gmail.com>
2026-01-09 11:25:29 +0200

    Added nodeBasics folder with index file (npm init + chalk installed)

---
+++ nodeBasics/index.js
@@ -0,0 +1,3 @@
+import chalk from 'chalk';
+
+console.log('Like Sponge Bob said, let\'s use our ' + chalk.red('I') + chalk.yellow('MA') + chalk.green('GI') + chalk.cyan('NA') + chalk.blue('TI') + chalk.magenta('ON'));
