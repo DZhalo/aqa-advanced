0110e89d4f6f663af5678caac0f796e5696c4809
Daria Zhaloba <d.zhaloba@gmail.com>
2026-01-19 11:26:31 +0200

    Added JS basics homework

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+let myString = "Lorem ipsum dolor..";
+let myNumber = 29;
+let myBoolean = true;
+let myNull = null;
+let myUndefined;
+
+console.log(myString);
+console.log(myNumber);
+console.log(myBoolean);
+console.log(myNull);
+console.log(myUndefined);
---
+++ jsBasics/task4.js
@@ -0,0 +1,16 @@
+//Площа кола
+let radius = 5;
+let circleArea = Math.PI * radius ** 2;
+console.log(circleArea.toFixed(2));
+
+//Площа прямокутника
+let length = 10;
+let width = 4;
+let rectangleArea = length * width;
+console.log(rectangleArea.toFixed(2));
+
+//Обʼєм циліндра
+let radiusCylinder = 3;
+let height = 7;
+let cylinderVolume = Math.PI * radiusCylinder ** 2 * height;
+console.log(cylinderVolume.toFixed(2));
